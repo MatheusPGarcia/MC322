503ac20459949a2635d36edbaf34a8d13aa2d3a7
Unknown <matheuspgarcia@gmail.com>
2018-08-07 14:33:14 -0300

    Adicionando classe Main

---
+++ Lab1/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Instanciando objetos
+		Card cardOne = new Card("First Example", 'S');
+		Card cardTwo = new Card("Second Example", 'A');
+		Card cardThree = new Card("Third Example", 'B');
+				
+		// Impressão dos objetos
+		System.out.println("Card one\n" + cardOne);
+		System.out.println("Card two\n" + cardTwo);
+		System.out.println("Card three\n" + cardThree);
+	}
+
+}
